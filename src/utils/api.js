b5987120bef5a684fb675902236df89171bb49f7
abhinab@clearout.io <abhinab@clearout.io>
2026-08-13 22:41:02 +0530

    changed base api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: "https://prenovabe.onrender.com/api"
-// });
-
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://prenovabe.onrender.com/api"
 });
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api"
+// });
 
 
 // 🔐 Attach token
